6624589ec52cc12f4cbc2bb5078020b6f106e900
Shaun Fuller <fullers@email.unc.edu>
2017-01-18 23:21:27 -0500

    fixed hash password and setup env for MogoDB

--- db/db.js
+++ db/db.js
@@ -3,17 +3,31 @@ var Promise = require("bluebird");
 mongoose.Promise = Promise;
 
 //DB
- var dbURI = 'mongodb://localhost/diabetictracker';
-if (process.env.NODE_ENV === 'production') {
-    dbURI= "";
-}
-mongoose.connect(dbURI);
-var db = mongoose.connection;
+//  var dbURI = 'mongodb://localhost/diabetictracker';
+// if (process.env.NODE_ENV === 'production') {
+//     dbURI= "";
+// }
+// mongoose.connect(dbURI);
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-  db.on("error", function(err) {
-    console.log("Mongoose Error: ", err);
-  });
+var config = require('../app/config/config.js');
+var mongoose = require('../app/config/mongoose.js');
+var express = require('express');
 
-  db.once("open", function() {
-    console.log("Mongoose connection successful.");
-  });
+var db = mongoose(),
+    app = express();
+
+app.listen(config.port);
+
+module.exports = app;
+console.log(process.env.NODE_ENV  + ' server running at http://localhost:' + config.port);
+
+// var db = mongoose.connection;
+
+//   db.on("error", function(err) {
+//     console.log("Mongoose Error: ", err);
+//   });
+
+//   db.once("open", function() {
+//     console.log("Mongoose connection successful.");
+//   });
