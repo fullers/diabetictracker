39c1929c1f1599385314aeb3b92c1f92698dac7a
Shaun Fuller <fullers@email.unc.edu>
2017-01-20 22:59:16 -0500

    Heroku Push

--- app/config/env/production.js
+++ app/config/env/production.js
@@ -2,5 +2,5 @@ var port = process.env.PORT;
 
 module.exports = {
 	port: port,
-	db: 'mongodb://'
+	db: 'mongodb://heroku_lx42pmmv:ikkntqg0p7327mb1uj80t71ktm@ds117849.mlab.com:17849/heroku_lx42pmmv'
 }
